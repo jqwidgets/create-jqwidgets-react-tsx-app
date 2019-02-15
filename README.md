c3a863f12cbf4264a477ce9ad2552308087eaa0d
jQWidgets <info@jqwidgets.com>
2019-02-15 14:50:15 +0200

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # create-jqwidgets-react-tsx-app
+Please refer to its documentation:
+  - [Getting Started](https://github.com/jqwidgets/create-jqwidgets-react-tsx-app/blob/master/README.md#getting-started) 
+  - [jQWidgets React TSX Documentation](http://www.jqwidgets.com/reactjs-components-documentation/)
+
+This project is based on [create-react-app](https://github.com/facebookincubator/create-react-app).
+
+## Quick Overview
+```sh
+npm i -g create-jqwidgets-react-tsx-app 
+
+create-jqwidgets-react-tsx-app my-app
+cd my-app
+npm start
+```
+
+When you’re ready to deploy to production, create a minified bundle with `npm run build`.
+
+## Getting Started
+
+### Installation
+
+Install it once globally:
+
+```sh
+npm install -g create-jqwidgets-react-tsx-app
+```
+
+**Make sure you have the latest version of [Node](https://nodejs.org/en/) on your machine**.
+
+### Creating an App
+
+```sh
+create-jqwidgets-react-tsx-app my-app
+cd my-app
+```
+
+It will create a directory called `my-app` inside the current folder.<br>
+Inside that directory, it will generate the initial project structure and install the transitive dependencies:
+
+```
+my-app
+├── node_modules
+├── public
+├── src
+├── package.json
+├── README.md
+├── tsconfig.json
+```
+
+Subfolders are not shown in order to keep it short.<br>
+Once the installation is done, you can run some commands inside the project folder:
+
+### `npm start`
+Builds the app in development mode and starts a web server. <br />
+The page will reload if you make edits.
+
+### `npm run build`
+Builds the app in production mode and stores it in the `build/` directory. <br />
+It correctly bundles React in production mode and optimizes the build for the best performance.  <br />
+By default, it also includes a service worker so that your app loads from local cache on future visits.
+
+## Why Use This?
+
+**If you’re getting started** with `jQWidgets` and `React`, using `create-jqwidgets-react-tsx-app` is the easiest way to do this.  <br />
+Just install, run and you will see an React jQWidgets Grid on your page. <br />
+Then you can customize the project as much as you want, making something great.
---
+++ src/App.tsx
@@ -0,0 +1,72 @@
+import * as React from 'react';
+
+import 'jqwidgets-scripts/jqwidgets/styles/jqx.base.css';
+import 'jqwidgets-scripts/jqwidgets/styles/jqx.material-purple.css';
+
+import JqxGrid, { IGridProps, jqx } from 'jqwidgets-scripts/jqwidgets-react-tsx/jqxgrid';
+
+class App extends React.PureComponent<{}, IGridProps> {
+
+    private source: IGridProps['source'] = {
+        localdata:
+            [
+                ['Maria Anders', 'Sales Representative', 'Berlin', 'Germany'],
+                ['Ana Trujillo', 'Owner', 'Mxico D.F.', 'Mexico'],
+                ['Antonio Moreno', 'Owner', 'Mxico D.F.', 'Mexico'],
+                ['Thomas Hardy', 'Sales Representative', 'London', 'UK'],
+                ['Christina Berglund', 'Order Administrator', 'Lule', 'Sweden'],
+                ['Hanna Moos', 'Sales Representative', 'Mannheim', 'Germany'],
+                ['Frdrique Citeaux', 'Marketing Manager', 'Strasbourg', 'France'],
+                ['Martn Sommer', 'Owner', 'Madrid', 'Spain'],
+                ['Owner', 'Marseille', 'France'],
+                ['Elizabeth Lincoln', 'Accounting Manager', 'Tsawassen', 'Canada'],
+                ['Victoria Ashworth', 'Sales Representative', 'London', 'UK'],
+                ['Patricio Simpson', 'Sales Agent', 'Buenos Aires', 'Argentina'],
+                ['Francisco Chang', 'Marketing Manager', 'Mxico D.F.', 'Mexico'],
+                ['Yang Wang', 'Owner', 'Bern', 'Switzerland'],
+                ['Pedro Afonso', 'Sales Associate', 'Sao Paulo', 'Brazil'],
+                ['Elizabeth Brown', 'Sales Representative', 'London', 'UK'],
+                ['Sven Ottlieb', 'Order Administrator', 'Aachen', 'Germany'],
+                ['Janine Labrune', 'Owner', 'Nantes', 'France'],
+                ['Ann Devon', 'Sales Agent', 'London', 'UK'],
+                ['Roland Mendel', 'Sales Manager', 'Graz', 'Austria']
+            ],
+        datafields:
+            [
+                { name: 'ContactName', type: 'string', map: '0' },
+                { name: 'Title', type: 'string', map: '1' },
+                { name: 'City', type: 'string', map: '2' },
+                { name: 'Country', type: 'string', map: '3' }
+            ],
+        datatype: 'array'
+    };
+
+    private columns: IGridProps['columns'] =
+        [
+            { text: 'Contact Name', datafield: 'ContactName', width: 240 },
+            { text: 'Contact Title', datafield: 'Title', width: 240 },
+            { text: 'City', datafield: 'City', width: 150 },
+            { text: 'Country', datafield: 'Country' }
+        ];
+
+    constructor(props: {}) {
+        super(props);
+
+        this.state = {
+            columns: this.columns,
+            source: new jqx.dataAdapter(this.source)
+        };
+    }
+
+    public render() {
+        return (
+            <JqxGrid
+                width={850} source={this.state.source} columns={this.state.columns}
+                pageable={true} autoheight={true} sortable={true} theme={'material-purple'}
+                altrows={true} enabletooltips={true} editable={true}
+            />
+        );
+    }
+}
+
+export default App;
